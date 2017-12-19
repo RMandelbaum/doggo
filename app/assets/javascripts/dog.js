d1ee4c181d1114b94ab8d7c0741996d23bcff6a0
Rachel Mandelbaum <rlmandelbaum@gmail.com>
2017-12-18 21:26:30 -0500

    edited index/prototype

--- app/assets/javascripts/dog.js
+++ app/assets/javascripts/dog.js
@@ -1,19 +1,29 @@
-function Dog(dog){
-    this.id = dog.id
-    this.name = dog.name
-    this.age = dog.age
-    this.breed = dog.breed
-    this.temperament = dog.temperament
-    this.walks = dog.walks
-    this.created_at = new Dog(dog.created_at)
+$(document).ready(function() {
 
-}
-//dog.prototype.?? = new Dog(dog)
+let dogs = []
+  class Dog{
+    constructor(dog){
+      this.id = dog.id
+      this.name = dog.name
+      this.age = dog.age
+      this.breed = dog.breed
+      this.temperament = dog.temperament
+      this.walks = dog.walks
+    }
+
+    render(){
+      //prototype
+      // renders html description of the dog
+      $("#dogs-list").addClass('container').append(`<img src= 'https://irp-cdn.multiscreensite.com/78cb147d/dms3rep/multi/mobile/dog%20walking-849x565.jpg'><a class="show-dog" href = '/dogs/${this.id}'>${this.name}</a><br>`)
+     }
+    }
 
 
-$(function (){
 
-  Dog.prototype.indexDogs = function(dog) {
+
+
+
+function indexDogs () {
   $('.list-dogs').click(function (event) {
       event.preventDefault();
       $(".pic").hide();
@@ -23,47 +33,63 @@ $(function (){
         type: 'GET',
       }).done(function(resp){
           resp.forEach(function(item){
-              //let dog = new Dog(item)
-            // debugger
-
-            console.log(item.name)
-            $("#dogs-list").addClass('container').append(`<li><img src= 'https://irp-cdn.multiscreensite.com/78cb147d/dms3rep/multi/mobile/dog%20walking-849x565.jpg'><a href = '/dogs/${item.id}' class="dogs">${item.name}</a></li><br>`)
-          })
+               let dog = new Dog(item)
+               dogs.push(dog)
+          //   $("#dogs-list").addClass('container').append(`<img src= 'https://irp-cdn.multiscreensite.com/78cb147d/dms3rep/multi/mobile/dog%20walking-849x565.jpg'><a class="show-dog" href = '/dogs/${item.id}'>${item.name}</a><br>`)
+            })
+            showAllDogs();
         })
     })
 
   }
 
-Dog.prototype.indexDogs();
+  // Dog.prototype.showDogs = new Dog()
+  //
+  function showDogs() {
+
+      $('.show-dog').click(function(e){
+      e.preventDefault()
+      debugger
+
+    })
+  }
+
+  function showAllDogs(){
+    dogs.forEach(function(dog){
+      dog.render();
+
+    })
+
+    showDogs();
+  }
 
 
 
-  function showDog(){
-      $('.dogs').on('click',function (event) {
-        event.preventDefault()
-        debugger
-         $(".info").hide()
-        // var id = this.id
-        // alert(id)
+      // $('.show-dog').click(function (event) {
+      //   debugger
+      //   event.preventDefault()
+      //   debugger
+      //    $(".info").hide()
+      //    var id = this.id
+      //    alert(id)
+      //
+      //   $.get("dogs/1", function(data){
+      //   $("#show-dog").append("<h4>Age: " + data["age"] + "</h4><h4>Breed: " + data["breed"] + "</h4><h4>Temperament: " + data["temperament"] + "</h4>")
+      //   $('#show-dog').append("<table><tr><th>Day</th><th>Time</th></tr></table>")
+      //
+      //    data.walks.forEach(function(walk){
+      //      $('#show-dog').append("<table><tr><td>" + walk["day"] + "</td><td>" + walk["time"] + "</td></tr></table")
+      //       })
+      //    })
+      //
+      //    $(".hide-me").hide();
+      //
+      //      showLess();
+      //    showComments();
 
-        $.get("dogs/1", function(data){
-        $("#show-dog").append("<h4>Age: " + data["age"] + "</h4><h4>Breed: " + data["breed"] + "</h4><h4>Temperament: " + data["temperament"] + "</h4>")
-        $('#show-dog').append("<table><tr><th>Day</th><th>Time</th></tr></table>")
 
-         data.walks.forEach(function(walk){
-           $('#show-dog').append("<table><tr><td>" + walk["day"] + "</td><td>" + walk["time"] + "</td></tr></table")
-            })
-         })
+    indexDogs();
 
-         $(".hide-me").hide();
-
-           showLess();
-         showComments();
-
-       })
-     }
-
-     showDog();
 
 
   function showLess(){
